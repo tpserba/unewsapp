ac72b6611e41048150b0932bf65be8b5097494f1
Tomás Praga <tpraga@serbatic.es>
2023-05-11 16:04:14 +0200

    setup page rendering and create first page

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const express = require('express');
+// Imports
+const express = require("express");
 const app = express();
 const PORT = 3003;
+const mustache = require("mustache-express")
+
+//Setup
+app.engine("mustache", mustache());
+app.set("views","./views");
+app.set("view engine", "mustache");
+
+
+//Start
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
