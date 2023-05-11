37a548f0552fe826108f059583748c5993177997
Tomás Praga <tpraga@serbatic.es>
2023-05-11 17:08:48 +0200

    implement user registration

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const express = require("express");
 const app = express();
 const PORT = 3003;
 const mustache = require("mustache-express")
-
+const bodyParser = require("body-parser");
 //Setup
 app.engine("mustache", mustache());
 app.set("views","./views");
 app.set("view engine", "mustache");
+// Tells body parser what body we're looking for
+app.use(bodyParser.urlencoded({extended:false}));
 
 
 //Start
@@ -15,6 +17,13 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
+app.post("/register", (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log(username);
+    console.log(password);
+    res.send("REGISTER")
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
